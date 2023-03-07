7726ad78fce424067a633afab04adefbe78caf3a
anniellano <c.andrea2086@gmail.com>
2023-03-07 10:07:59 -0500

    Solución taller

--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -1,12 +1,21 @@
 package classroom;
 
 public enum Tipo {
-	DISCIPLINAR(10),FUNDAMENTACION(20),ELECTIVA(30);
-	
-	private int codigo;
-	private String nombre;
-	public Tipo(int codigo,String nombre) {
+	DISCIPLINAR(10, "Disciplinar"),FUNDAMENTACION(20, "Fundamentación"),ELECTIVA(30, "Electiva");
+
+	private final int codigo;
+	private final String nombre;
+        
+	Tipo(int codigo,String nombre) {
 		this.codigo = codigo;
 		this.nombre = nombre;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
 	}
 }
